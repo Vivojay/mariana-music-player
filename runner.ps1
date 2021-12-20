2883e3cf2d8844d30ba7adfaca2268c0fbe7909d
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2021-12-20 16:54:53 +0530

    Delete runner.ps1

--- runner.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location "D:\OneDrive - Suncity School\My Stuff\College\--Academics--\--B Tech--\--Year 1--\_Main_\DA1001\Project SEM-I\Music Player\src\CLI"
-..\..\venv\scripts\activate
-py -3.9 main2.py
