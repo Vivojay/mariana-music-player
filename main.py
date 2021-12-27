a987fcf78f7df443895d6a76d6275a268f89fa13
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2021-12-27 23:38:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ except ImportError:
     print("[INFO] Skipped 17/24")
 
 try:
-    import librosa.beat.beat_track, librosa.load
+    import librosa
     print("Loaded 18/24",  end='\r') # Time taking import (Sometimes, takes ages...)
 except ImportError:
     print("[WARN] Could not load music computation extension...")
