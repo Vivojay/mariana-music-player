aa2896008bb8b274928b7f4470e8735f146a2286
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2021-12-31 16:43:06 +0530

    Made a bit more useful...

--- logger.py
+++ logger.py
@@ -23,6 +23,7 @@ logleveltypes = {
     4: "debug"
 }
 
+
 def NOW():
     return dt.strftime(dt.now(), '%d-%b-%Y %H:%M:%S')
 
@@ -31,7 +32,7 @@ def SAY(
     log_priority, # Default value is defined in main.py
     display_message = None, # Displayed on app
     log_message: str = '', # Saved to log file
-    out_file = 'logs/generallogs.log', # Log file path
+    out_file = 'logs/general.log', # Log file path
     format_style: int = 2,
 ):
     global loglevel, logleveltypes
@@ -46,17 +47,19 @@ def SAY(
     writemode = writemodes[os.path.exists(out_file)]
 
     if log_priority == 1: # Fatal crash logs must be saved in logs/appcrashes.log also...
-        with open("logs/appcrashes.log", ['w', 'a'][os.path.isfile("logs/appcrashes.log")]) as crash_log_file:
+        with open("logs/appcrashes.log", ['w', 'a'][os.path.isfile("logs/appcrashes.log")], encoding="utf-8") as crash_log_file:
             crash_log_file.write()
 
     if log_priority:
-        with open(out_file, writemode) as logfile:
-            if format_style == 1:
+        with open(out_file, writemode, encoding="utf-8") as logfile:
+            if format_style == 0:
+                formatted_log_message = log_message+"\n"
+            elif format_style == 1:
                 formatted_log_message = f"({llt}) {' '*20} => {log_message}\n"
             elif format_style == 2:
                 formatted_log_message = f"({llt}) {NOW()} => {log_message}\n"
             else:
-                raise "InvalidLogformat_styleError"
+                raise ValueError("InvalidLogformat_styleError")
 
-            if format_style in range(1, 3):
+            if format_style in range(0, 3):
                 logfile.write(formatted_log_message)
