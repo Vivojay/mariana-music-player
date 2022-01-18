033ff68fcebea2924b195fdf608b4ae6cd962e7e
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2022-01-18 22:13:12 +0530

    Update get_lyrics.py

--- lyrics_provider/get_lyrics.py
+++ lyrics_provider/get_lyrics.py
@@ -20,7 +20,7 @@ os.chdir(curdir)
 SUPPORTED_FILE_TYPES = SYSTEM_SETTINGS["system_settings"]["supported_file_types"]
 FOOT_TEXT = "Lyrics Powered by Musixmatch"
 
-def get_lyrics(songfile=None, weblink=None, isYT=False):
+def get_lyrics(max_wait_lim, songfile=None, weblink=None, isYT=False):
     """
     `lyr`: An intermediate string which is processed into `text_to_be_displayed`
     `head_text` and `text_to_be_displayed` are the important final results
@@ -31,35 +31,36 @@ def get_lyrics(songfile=None, weblink=None, isYT=False):
     head_text = "Lyrics N/A"
     text_to_be_displayed = "(Lyrics not available)"
 
-    if songfile:
+    if weblink:
+        SONG_INF=lyrics_provider.detect_song.get_weblink_audio_info(max_wait_lim=max_wait_lim, weblink=weblink, isYT=isYT)
+    elif songfile:
         if not songfile.endswith(tuple(SUPPORTED_FILE_TYPES)):
             return (text_to_be_displayed, head_text)
-
-    if weblink:
-        SONG_INF=lyrics_provider.detect_song.get_weblink_audio_info(weblink, isYT=isYT)
-    elif songfile:
         SONG_INF=lyrics_provider.detect_song.get_song_info(songfile)
     else:
-        SONG_INF = []
+        SONG_INF = {}
 
-    if SONG_INF != []:
+    if SONG_INF != {}:
         head_text = SONG_INF['display_name']
         lyr = SONG_INF.get('lyrics')
         if lyr:
             lyr = '\n'.join(lyr)
             text_to_be_displayed = lyr
-    
+
     return (text_to_be_displayed, head_text)
 
 
-def show_window(songfile=None, weblink=None, isYT=False):
+def show_window(max_wait_lim, songfile=None, weblink=None, isYT=False):
 
-    text_to_be_displayed, head_text = get_lyrics(songfile=songfile, weblink=weblink, isYT=isYT)
+    text_to_be_displayed, head_text = get_lyrics(songfile=songfile, weblink=weblink, isYT=isYT, max_wait_lim=max_wait_lim)
 
     root = tk.Tk()
     root.resizable(True, True)
     root.geometry("550x270")
     root.title("Mariana - Lyrics Window")
+
+    os.chdir(curdir)
+    os.chdir('..')
 
     # Icon for lyrics window
     p1 = tk.PhotoImage(file = 'res/lyrics_icon.png')
@@ -69,7 +70,6 @@ def show_window(songfile=None, weblink=None, isYT=False):
     root.grid_columnconfigure(0, weight=1)
     root.grid_rowconfigure(0, weight=1)
     root.resizable=((0, 0)) # Disable window resizing
-
 
     head = tk.Label(root, text=head_text, font=("Segoe UI Bold", 14))
     foot = tk.Label(root, text=FOOT_TEXT, font=("Arial Italic", 10), fg='brown')
@@ -98,7 +98,3 @@ def show_window(songfile=None, weblink=None, isYT=False):
     text['yscrollcommand'] = scrollbar.set
 
     root.mainloop()
-
-
-if __name__ == '__main__':
-    show_window(r'D:\FL Utils\Downloaded Songs\undo.wav')
