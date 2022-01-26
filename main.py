67610e6f1bf84bb458ab23c677d583e4462d6302
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2022-01-26 13:32:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -358,9 +358,9 @@ def reload_sounds(quick_load = True):
         _sound_files_names_only = [os.path.splitext(os.path.split(i)[1])[0] for i in _sound_files]
         _sound_files_names_enumerated = [(i+1, j) for i, j in enumerate(_sound_files_names_only)]
 
-if FIRST_BOOT:
-    with open('data/snd_files.json', 'w', encoding='utf-8') as fp:
-        json.dump(_sound_files, fp)
+        if FIRST_BOOT:
+            with open('data/snd_files.json', 'w', encoding='utf-8') as fp:
+                json.dump(_sound_files, fp)
 
 reload_sounds(quick_load = not FIRST_BOOT) # First boot requires quick_load to be disabled,
                                            # other boots can do away with quick_loads :)
@@ -554,19 +554,20 @@ def voltransition(
 ):
     global cached_volume, current_media_player
 
-    if current_media_player:
-        for i in range(101):
-            diffvolume = initial+(final-initial)*i
-            time.sleep(transition_time/100)
-            vas.vlc_media_player.get_media_player().audio_set_volume(int(diffvolume))
-    else:
-        for i in range(101):
-            diffvolume = initial+(final-initial)*i/100
-            time.sleep(transition_time/100)
-            pygame.mixer.music.set_volume(round(diffvolume, 2))
+    if not ismuted:
+        if current_media_player:
+            for i in range(101):
+                diffvolume = initial+(final-initial)*i
+                time.sleep(transition_time/100)
+                vas.vlc_media_player.get_media_player().audio_set_volume(int(diffvolume))
+        else:
+            for i in range(101):
+                diffvolume = initial+(final-initial)*i/100
+                time.sleep(transition_time/100)
+                pygame.mixer.music.set_volume(round(diffvolume, 2))
 
-    # if not disablecaching:
-    #     cached_volume = final
+        # if not disablecaching:
+        #     cached_volume = final
 
 
 def vol_trans_process_spawn():
@@ -1159,7 +1160,7 @@ def lyrics_ops(show_window):
     refresh_lyrics = not (lyrics_saved_for_song == currentsong) # Song has changed since last save of lyrics,
                                                                 # need to refresh the lyrics to match the current song
     get_related = SETTINGS['get related songs']
-    if current_media_player: # FIXME: Broken
+    if current_media_player:
         if current_media_type == 0:
             IPrint(f"Loading lyrics window for YT stream (Time taking)...", visible=visible)
             get_lyrics.show_window(refresh_lyrics = refresh_lyrics,
@@ -1238,14 +1239,8 @@ def process(command):
             return False
 
         if commandslist == ['all']:
-            if visible:
-                results_enum = enumerate(_sound_files_names_only)
-                IPrint(tbl([(i+1, j) for i, j in results_enum], tablefmt='plain'), visible=visible)
-            else:
-                SAY(visible=True,
-                display_message="Turn on visibility to access this command",
-                log_message="Accessing all command with visibility switched off",
-                priority=3)
+            results_enum = enumerate(_sound_files_names_only)
+            IPrint(tbl([(i+1, j) for i, j in results_enum], tablefmt='plain'), visible=visible)
 
         # TODO: Need to display files in n columns (Mostly 3 cols) depending upon terminal size (dynamically...)
         if commandslist[0] in ['list', 'ls']:
@@ -1373,6 +1368,9 @@ def process(command):
             last_index, last_name = _sound_files_names_enumerated
             IPrint(">| ", visible=visible)
 
+        if commandslist in [['hist', 'count'], ['history', 'count']]:
+            IPrint(f"History count: {len(HISTORY_QUEUE)}", visible=visible)
+
         elif commandslist[0] in ['hist', 'history']:
             # TODO: Get values for `order_results` and `order_type` from SETTINGS
             indices = [] # Indices of songs to be displayed
@@ -1470,16 +1468,20 @@ def process(command):
                     log_priority=2)
 
         elif commandslist == ['reload']:
+            IPrint("Refreshing lyrics", visible=visible)
+            lyrics_ops(show_window=False)
             IPrint("Reloading sounds", visible=visible)
             reload_sounds(quick_load = False)
-            IPrint("Done...", visible=visible)
-        
+            IPrint(f"Loaded {len(_sound_files)}", visible=visible)
+
         elif commandslist == ['refresh']:
+            IPrint("Refreshing lyrics", visible=visible)
+            lyrics_ops(show_window=False)
             IPrint("Reloading sounds", visible=visible)
             reload_sounds(quick_load = False)
             IPrint("Reloading settings", visible=visible)
             refresh_settings()
-            IPrint("Done...", visible=visible)
+            IPrint(f"Loaded {len(_sound_files)}", visible=visible)
 
         elif commandslist == ['vis']:
             visible = not visible
@@ -2411,6 +2413,13 @@ def run():
     save_user_data()
 
     pygame.mixer.init()
+
+    if FIRST_BOOT:
+        startup_sound_path = "res/first_boot_startup_sound.mp3"
+        if os.path.isfile(startup_sound_path):
+            pygame.mixer.music.load(startup_sound_path)
+            pygame.mixer.music.play()
+
     if visible: showbanner()
     mainprompt()
 
