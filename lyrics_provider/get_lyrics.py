d13f4e20395c4dc811ef6836d56d0d5f8373e3cf
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2022-02-01 00:42:02 +0530

    Update get_lyrics.py

--- lyrics_provider/get_lyrics.py
+++ lyrics_provider/get_lyrics.py
@@ -7,7 +7,11 @@ curdir = os.path.dirname(__file__)
 os.chdir(curdir)
 
 import lyrics_provider.detect_song
+
 from tkinter import ttk
+from ruamel.yaml import YAML
+
+yaml = YAML(typ='safe')
 
 try:
     with open("settings/system.toml", encoding="utf-8") as file:
@@ -15,10 +19,17 @@ try:
 except IOError:
     SYSTEM_SETTINGS = None
 
+try:
+    with open("settings/settings.yml", encoding="utf-8") as file:
+        SETTINGS = yaml.load(file)
+except IOError:
+    SETTINGS = None
+
 os.chdir(curdir)
 
 SUPPORTED_FILE_TYPES = SYSTEM_SETTINGS["system_settings"]["supported_file_types"]
 FOOT_TEXT = "Lyrics Powered by Musixmatch"
+LYRICS_SETTINGS = SETTINGS['lyrics']
 
 def get_lyrics(max_wait_lim, get_related, songfile=None, weblink=None, isYT=False):
     """
@@ -72,6 +83,9 @@ def create_lyrics_html():
                     '    <meta name="viewport" content="width=device-width, initial-scale=1.0">',
                     '    <link rel="stylesheet" href="../res/style.css">',
                     '    <link rel="preload" href="Elsie-Regular.ttf" as="font" type="font/ttf" crossorigin>',
+                    '    <link rel="preconnect" href="https://fonts.googleapis.com">'
+                    '    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>'
+                    '    <link href="https://fonts.googleapis.com/css2?family=Arima+Madurai:wght@500&display=swap" rel="stylesheet">'
                     '</head>'
         ]
 
@@ -90,6 +104,56 @@ def show_window(max_wait_lim, show_window, get_related, refresh_lyrics = True, s
 
     if refresh_lyrics:
         text_to_be_displayed, head_text = get_lyrics(songfile=songfile, get_related=get_related, weblink=weblink, isYT=isYT, max_wait_lim=max_wait_lim)
+
+        # Create CSS file from default.css and the provided lyrics wallpaper image name (from settings file)
+
+        with open("res/default.css", 'r', encoding='utf-8') as default_css_file:
+            default_css = default_css_file.read()
+
+        body_css = 'body {\n'
+        DEFAULT_BG_PATH = 'res/lyrics-wallpapers/DEFAULT.jpg'
+
+        # Using a solid color wallpaper
+        if LYRICS_SETTINGS['use solid color bg']:
+            body_css += '  background-color: {0};\n'.format(LYRICS_SETTINGS['solid color bg']['color'])
+
+        # Using an image wallpaper
+        else:
+            # Initialize lyrics bg image path to the DEFAULT wallpaper (DEFAULT.jpg)
+            lyrics_bg_image_abs_path = DEFAULT_BG_PATH
+            lyrics_bg_image_dir = LYRICS_SETTINGS['webview wallpaper']['wallpaper folder']
+
+            if lyrics_bg_image_dir and os.path.isdir(lyrics_bg_image_dir): # Wallpaper directory is explicitly provided
+                lyrics_bg_image_file = LYRICS_SETTINGS['webview wallpaper']['wallpaper name']
+
+                if lyrics_bg_image_file:
+                    lyrics_bg_image_file = os.path.join(lyrics_bg_image_dir, lyrics_bg_image_file)
+                    if os.path.isfile(lyrics_bg_image_file):
+                        lyrics_bg_image_abs_path = os.path.join(lyrics_bg_image_dir, lyrics_bg_image_file)
+
+            else: # Wallpaper directory reverts to default
+                lyrics_bg_image_file = LYRICS_SETTINGS['webview wallpaper']['wallpaper name']
+
+                if lyrics_bg_image_file and os.path.isfile(lyrics_bg_image_file):
+                    lyrics_bg_image_abs_path = os.path.join(lyrics_bg_image_dir, lyrics_bg_image_file)
+
+
+            body_css += \
+                '  background-image: linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.3), rgba(255, 255, 255, 0.2)),\n'\
+                '  url("{0}");\n\n'\
+                \
+                '  background-size: cover;\n'\
+                '  background-repeat: no-repeat;\n'\
+                '  background-position: center bottom;\n'\
+                '  background-attachment: fixed;'.format(lyrics_bg_image_abs_path.replace('\\', '/'))
+        
+        body_css += '\n  }'
+
+        default_css += body_css
+
+        with open("res/style.css", 'w', encoding='utf-8') as css_file:
+            css_file.write(default_css)
+
         # try:
         # os.path.isdir('../temp/')
         with open('temp/lyrics.txt', 'w', encoding='utf-8') as fp:
