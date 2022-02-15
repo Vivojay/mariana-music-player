f02535aea9acd2543f5303daa58b59e0765d328e
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2022-02-15 20:38:02 +0530

    Refreshed colour theme to "underwater"

--- lyrics_provider/lyrics_window_spawn.py
+++ lyrics_provider/lyrics_window_spawn.py
@@ -10,6 +10,7 @@ def spawn_lyrics_window(text_to_be_displayed, head_text, foot_text):
     root.resizable(True, False)
     root.geometry("550x270")
     root.title("Mariana - Lyrics Window")
+    root.config(bg="#1a1a1a")
 
     # Icon for lyrics window
     p1 = tk.PhotoImage(file = '../res/lyrics_icon.png')
@@ -20,8 +21,8 @@ def spawn_lyrics_window(text_to_be_displayed, head_text, foot_text):
     root.grid_rowconfigure(0, weight=1)
     root.resizable=((0, 0)) # Disable window resizing
 
-    head = tk.Label(root, text=head_text, font=("Segoe UI Bold", 14))
-    foot = tk.Label(root, text=foot_text, font=("Arial Italic", 10), fg='brown')
+    head = tk.Label(root, text=head_text, font=("Segoe UI Bold", 14), fg="orange", bg="#1a1a1a")
+    foot = tk.Label(root, text=foot_text, font=("Arial Italic", 10), fg='brown', bg="#1a1a1a")
 
     head.grid(row=0, column=0, sticky='new')
     foot.grid(row=2, column=0, sticky='sew')
@@ -32,13 +33,14 @@ def spawn_lyrics_window(text_to_be_displayed, head_text, foot_text):
     text.delete(1.0, "end")
     text.insert('end', '\n'+text_to_be_displayed)
 
-    text.config(state='disabled', font = ("Segoe UI", 11))
+    text.config(state='disabled', font = ("Segoe UI", 11), fg="#ffdead", bg="#000000")
     text.grid(row=1, column=0, sticky='new')
 
     # A previous plan (Using the MusixMatch API to get time-synced "rich lyrics"??)
     # location = settings['general']['current_location']
     # if location == 'JP': # Japan users have an agreement with Musixmatch to NOT BE ALLOWED TO COPY / PASTE LYRICS using their API
     #     text.bindtags((str(text), str(root), "all"))
+
 
     # Create a scrollbar widget and set its command to the text widget
     scrollbar = ttk.Scrollbar(root, orient='vertical', command=text.yview)
